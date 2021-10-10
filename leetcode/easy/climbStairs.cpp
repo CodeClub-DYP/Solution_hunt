5041e2204a046966f28d667d191b37993c50c163
yashladekar <76871508+yashladekar@users.noreply.github.com>
2021-10-10 20:29:33 +0530

    add comments

--- leetcode/easy/climbStairs.cpp
+++ leetcode/easy/climbStairs.cpp
@@ -3,6 +3,16 @@ using namespace std;
 class Solution
 {
 public:
+     /*
+    the solution of this from is based on the concept of the fibonacci number
+    let us concider that the person is moving down the staires
+    each round you can choose 1 step or 2 steps
+    thus, f(x) = f(x-1) + f(x-2)
+    initial condition: f(1) = 1 and f(2) = 2
+    hence, f(3) = f(2) + f(1) = 3
+    f(4) = f(3) + f(2) = 3 +2 =5
+    Therefore, the serie is like: 1, 2, 3, 5, 8, 13, ....
+    */
     int climbStairs(int n)
     {
         int previous = 2, previous2 = 1;
